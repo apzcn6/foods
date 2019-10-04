c29287c10035f170b02deae7e5025d97321c9396
Andrew Ziber <apzcn6@mail.missouri.edu>
2019-10-04 16:18:19 -0500

    finish table view

---
+++ Foods/foodsTests/foodsTests.swift
@@ -0,0 +1,34 @@
+//
+//  foodsTests.swift
+//  foodsTests
+//
+//  Created by Andrew Ziber on 10/2/19.
+//  Copyright © 2019 Andrew Ziber. All rights reserved.
+//
+
+import XCTest
+@testable import foods
+
+class foodsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
